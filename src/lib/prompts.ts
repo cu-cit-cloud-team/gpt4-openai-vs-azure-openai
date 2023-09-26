b375fc45add6f2c08cff60b6f59533d83ec9d4b6
Michael Sprague <mikesprague@gmail.com>
2023-09-26 18:47:12 -0400

    refactor(lib/prompts): add prompt

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -28,4 +28,20 @@ export const allPromptsToTest: Prompt[] = [
       content: 'Why are the dinosaurs extinct?',
     },
   ],
+  [
+    {
+      role: 'system',
+      content: oneLineTrim`
+        You are an AI programming assistant. Follow the user's requirements
+        carefully and to the letter. First, think step-by-step and describe
+        your plan for what to build in pseudocode, written out in great detail.
+        Then, output the code in a single code block. Minimize any other prose.
+      `,
+    },
+    {
+      role: 'user',
+      content:
+        'Solve the FizzBuzz problem using modern es2020 style JavaScript.',
+    },
+  ],
 ];
